d65eefa0e942b69ce1e6673faae4e3d3ccc0b514
samueldashadrach <samuel.da.shadrach@gmail.com>
2023-03-16 15:39:32 +0530

    created direct flask app

---
+++ flask-app-direct.py
@@ -0,0 +1,56 @@
+import flask
+import string
+import random
+import os
+import example.py
+
+
+app = flask.Flask(__name__)
+
+# runs when app starts
+gen_global = load(
+	ckpt_dir = "../weights/7B",
+	tokenizer_path = "../weights/tokenizer.model",
+	max_seq_len = 512,
+	max_batch_size = 32,
+	use_int8 = True
+	)
+
+
+
+@app.route("/")
+def hello():
+	print("flask server is running", flush=True)
+	return "flask server is running"
+
+@app.route("/flask-inference/", methods = ["POST"])
+def flask_inference_no_batching():
+
+	print("received POST request", flush=True)
+
+	req_data = flask.request.json
+	print("request data", req_data, flush=True) # for testing
+
+	apikey, prompt = req_data["apikey"], req_data["prompt"]
+	print(req_data["apikey"], req_data["prompt"], flush = True) # for testing
+
+	prompts = [prompt]
+	results = gen_global.generate(
+		prompts,
+		max_gen_len=1024,
+        temperature=temperature,
+        top_p=top_p,
+        repetition_penalty_range=repetition_penalty_range,
+        repetition_penalty_slope=repetition_penalty_slope,
+        repetition_penalty=repetition_penalty, 
+		)
+	result = results[0]
+
+	print("result from model: ", result)
+
+	res_data = {
+		"apikey": req_data["apikey"],
+		"result": result
+	}
+
+	return flask.jsonify(res_data)
